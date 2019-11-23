677c022427342cf2795b9c1bad6d079c9a07ed94
Mardoniosc <Mardonio@live.com>
2019-11-23 13:25:22 -0300

    Finalizado o projeto exemplo de react

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    "react/static-property-placement": 'off'
   }
 };
